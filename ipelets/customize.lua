36c8cc1d7b309a8583f55ce4d2f65a9f7bcd648c
Jc7185 <johnathancurrah8@gmail.com>
2026-01-20 17:18:43 +0000

    update version 1.2

---
+++ ipelets/customize.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------------------
+-- Customized ipelet: customize.lua
+----------------------------------------------------------------------
+
+-- Optionally set autosave interval
+-- prefs.autosave_interval = 10
+prefs.max_zoom = 100
+shortcuts.ipelet_1_symbols = "Shift+S"
+shortcuts.mode_splines = "Alt+Ctrl+I"
+prefs.scroll.vertical_sign = -1
+
+shortcuts["horizontalalignment|left"] = "Alt+Shift+L"
+shortcuts["horizontalalignment|hcenter"] = "Alt+Shift+H"
+shortcuts["horizontalalignment|right"] = "Alt+Shift+R"
+
+shortcuts["verticalalignment|bottom"] = "Alt+Shift+B"
+shortcuts["verticalalignment|baseline"] = "Alt+B"
+shortcuts["verticalalignment|vcenter"] = "Alt+Shift+C"
+shortcuts["verticalalignment|top"] = "Alt+Shift+T"
+
+-- Substitute with absolute /path/to/ElectricalEngineering.isy
+prefs.styles = { "~/.ipe/styles/ElectricalEngineering.isy" }
